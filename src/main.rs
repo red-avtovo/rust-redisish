7414bba11cdaf6190dc741fb2f5b4e96b539872a
Aleksandr Bochev <red.avtovo@gmail.com>
2018-10-10 17:00:11 +0200

    added multithreading support

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::net::{TcpListener, TcpStream};
 use std::io;
 use std::io::*;
+use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 #[derive(Debug)]
 enum Command {
     PUBLISH(Vec<String>),
     RETRIEVE,
-    NONE
+    QUIT,
+    NONE,
 }
 //echo "PUBLISH info one, info 2, Grüße, Jürgen ❤" | nc 127.0.0.1 8080
 //echo "RETRIEVE" | nc localhost 8080
@@ -19,54 +22,58 @@ macro_rules! log_if_error {
     }
 }
 
-fn handle_client(stream: TcpStream, data: &mut Vec<String>) {
+fn handle_client(stream: TcpStream, data: &Arc<Mutex< Vec<String>>>) {
     use Command::*;
     let mut reader = BufReader::new(&stream);
     let mut writer = BufWriter::new(&stream);
-    let mut buf = String::new();
-    log_if_error!(reader.read_line(&mut buf));
+    loop {
+        let mut buf = String::new();
+        if let Err(_) = reader.read_line(&mut buf) { break; }
 
-    let command = get_command(buf);
-    println!("Received command: {:?}", command);
-    match command {
-        PUBLISH(strings) => {
-            println!("publish: {:?}", strings);
-            strings.iter().for_each(|s| data.push(s.clone()))
-        },
-        RETRIEVE => {
-            match data.pop() {
-                Some(d) => {
-                    println!("retrieved: {}", d);
-                    log_if_error!(writer.write(format!("{}\n", d).as_bytes()));
-                },
-                None => {
-                    println!("Nothing left in the storage");
-                }
+        let command = get_command(buf);
+        println!("Received command: {:?}", command);
+        match command {
+            PUBLISH(strings) => {
+                println!("publish: {:?}", strings);
+                strings.iter().for_each(|s| data.lock().unwrap().push(s.clone()))
             }
-        },
-        _ => {}
-    };
+            RETRIEVE => {
+                match data.lock().unwrap().pop() {
+                    Some(d) => {
+                        println!("retrieved: {}", d);
+                        log_if_error!(writer.write(format!("{}\n", d).as_bytes()));
+                        log_if_error!(writer.flush());
+                    }
+                    None => {
+                        println!("Nothing left in the storage");
+                    }
+                }
+            },
+            QUIT => break,
+            _ => {}
+        };
+    }
 }
 
 fn get_command(command: String) -> Command {
     use Command::*;
     match &command.to_lowercase().trim() as &str {
         c if c.starts_with("publish ") => {
-            return PUBLISH(command["publish ".len()..].split(",").map(|s| s.trim().into()).collect())
-        },
-        "retrieve" => {
-            return RETRIEVE
-        },
-        _ => return NONE
+            PUBLISH(command["publish ".len()..].split(",").map(|s| s.trim().into()).collect())
+        }
+        "retrieve" => RETRIEVE,
+        "" => QUIT,
+        _ => NONE
     }
 }
 
-fn main() -> io::Result<()>{
+fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
-    let mut data: Vec<String> = Vec::new();
+    let atomic_data:Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
     // accept connections and process them serially
     for stream in listener.incoming() {
-        handle_client(stream?, &mut data);
+        let arc = Arc::clone(&atomic_data);
+        thread::spawn(move || handle_client(stream.unwrap(), &arc));
     }
     Ok(())
 }
