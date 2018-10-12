b933db278639d3d9cb0b21b525c30ada9b32dec3
Aleksandr Bochev <red.avtovo@gmail.com>
2018-10-12 12:00:45 +0200

    almost fixed the pool refactored redis component

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,25 @@
 extern crate core;
 
 use std::io;
-use std::io::*;
-use std::net::{TcpListener, TcpStream};
+use std::net::TcpListener;
 use std::sync::{Arc, Mutex};
 
 mod task;
-use task::pool::pool::Pool;
+mod redis;
+use task::pool::pool;
+use redis::ish::ish::handle_client;
 
-#[derive(Debug)]
-enum Command {
-    PUBLISH(Vec<String>),
-    RETRIEVE,
-    QUIT,
-    NONE,
-}
 //echo "PUBLISH info one, info 2, Grüße, Jürgen ❤" | nc 127.0.0.1 8080
 //echo "RETRIEVE" | nc localhost 8080
-
-macro_rules! log_if_error {
-    ($e:expr) => {
-        if let Err(e) = $e {
-            eprint!("There is an error happened: {}", e)
-        }
-    }
-}
-
-fn handle_client(stream: TcpStream, data: &Arc<Mutex<Vec<String>>>) {
-    use Command::*;
-    let mut reader = BufReader::new(&stream);
-    let mut writer = BufWriter::new(&stream);
-    loop {
-        let mut buf = String::new();
-        if let Err(_) = reader.read_line(&mut buf) { break; }
-
-        let command = get_command(buf);
-        println!("Received command: {:?}", command);
-        match command {
-            PUBLISH(strings) => {
-                println!("publish: {:?}", strings);
-                strings.iter().for_each(|s| data.lock().unwrap().push(s.clone()))
-            }
-            RETRIEVE => {
-                match data.lock().unwrap().pop() {
-                    Some(d) => {
-                        println!("retrieved: {}", d);
-                        log_if_error!(writer.write(format!("{}\n", d).as_bytes()));
-                        log_if_error!(writer.flush());
-                    }
-                    None => {
-                        println!("Nothing left in the storage");
-                    }
-                }
-            }
-            QUIT => break,
-            _ => {}
-        };
-    }
-}
-
-fn get_command(command: String) -> Command {
-    use Command::*;
-    match &command.to_lowercase().trim() as &str {
-        c if c.starts_with("publish ") => {
-            PUBLISH(command["publish ".len()..].split(",").map(|s| s.trim().into()).collect())
-        }
-        "retrieve" => RETRIEVE,
-        "" => QUIT,
-        _ => NONE
-    }
-}
 
 fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
     let atomic_data: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
-    let pool = Pool::new(10);
+    let pool = pool::new(10);
     // accept connections and process them serially
     for stream in listener.incoming() {
         let arc = Arc::clone(&atomic_data);
-        pool.exec( Box::new(move || handle_client(stream.unwrap(), &arc)))
+        pool::exec(&pool,Box::new(move || handle_client(stream.unwrap(), &arc)))
     }
     Ok(())
 }
--- src/task/pool.rs
+++ src/task/pool.rs
@@ -1,58 +1,48 @@
 pub mod pool {
-    use std::sync::mpsc::{channel, Sender, Receiver};
+    use std::sync::mpsc::{channel, Receiver, Sender};
     use std::thread;
-    use std::thread::JoinHandle;
+
+    type F = FnOnce() + Send;
 
     pub struct PoolsThread {
         id: i32,
         busy: bool,
-        task_sender: Sender<Box<FnOnce() + Send>>,
-        handle: Box<JoinHandle<()>>,
+        task_sender: Sender<Box<F>>,
     }
 
     pub struct Pool {
         threads: Vec<PoolsThread>
     }
 
-    impl Pool {
 
-        fn init_thread(n: i32) -> PoolsThread {
-            println!("initiating {} thread in the pool", n);
-            let mut busy: bool = false;
-            let (tx, rx): (Sender<Box<FnOnce() + Send + 'static>>, Receiver<Box<FnOnce() + Send + 'static>>) = channel();
-            let handle = Box::new(thread::spawn( move || {
-                loop {
-                    match rx.recv() {
-                        Ok(task) => {
-                            busy = true;
-                            task();
-                            busy = false;
-                        }
-                        _ => {}
-                    }
-                }
-            }));
-            PoolsThread{
-                id: n,
-                busy,
-                task_sender: tx,
-                handle
-            }
-        }
+    fn init_thread(n: i32) -> PoolsThread {
+        println!("initiating {} thread in the pool", n);
+//        let mut busy: bool = false;
+        let (tx, rx): (Sender<Box<F>>, Receiver<Box<F>>) = channel();
+        thread::spawn(move || rx.recv().map(|f| (f)()).unwrap());
 
-        pub fn new(count: i32) -> Self {
-            Pool {
-                threads: (0..count).map(|n| Self::init_thread(n)).collect()
-            }
+        PoolsThread {
+            id: n,
+            busy: false,
+            task_sender: tx,
         }
+    }
 
-        pub fn exec<F>(&self, f: Box<F>)
-        where F: 'static + Send + FnOnce()
-        {
-            let free_thread = self.threads.iter()
-                .filter(|t| t.busy == false)
-                .next().expect("There is no free threads!");
-            free_thread.task_sender.send(f);
+
+    pub fn new(count: i32) -> Pool {
+        Pool {
+            threads: (0..count).map(|n| init_thread(n)).collect()
         }
+    }
+
+    pub fn exec<F>(p: &Pool, _f: Box<F>)
+        where F: FnOnce() + Send + 'static
+    {
+        let free_thread = p.threads.iter()
+            .filter(|t| t.busy == false)
+            .next().expect("There is no free threads!");
+        println!("Executing task on thread #{}", free_thread.id);
+        let f = Box::new(|| println!("yo"));
+        free_thread.task_sender.send(f).expect("Unable to run the task");
     }
 }
